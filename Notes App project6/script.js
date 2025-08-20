2cb58288ff4f0d5c0efe3dba705c666d97fcc416
Yaser Qasemi <yaserqasemi599@gmail.com>
2025-08-20 16:29:05 +0430

    first commit

--- Notes App project6/script.js
+++ Notes App project6/script.js
@@ -41,5 +41,6 @@ document.addEventListener("keydown", event => {
     if (event.key === "Enter") {
         document.execCommand("insertLineBreak");
         event.preventDefault();
+        updateStorage();
     }
 })
