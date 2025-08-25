208a104fc2e49a9c96440d9d0613188ee9b92d63
Yaser Qasemi <yaserqasemi599@gmail.com>
2025-08-25 15:35:43 +0430

    second commit

---
+++ Text to speech 9/script.js
@@ -0,0 +1,25 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0]; // set default voice
+
+    // clear old options
+    voiceSelect.innerHTML = "";
+
+    voices.forEach((voice, i) => {
+        voiceSelect.add(new Option(voice.name, i));
+    });
+};
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+})
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    speech.voice = voices[voiceSelect.value]; // set voice from dropdown
+    window.speechSynthesis.speak(speech);
+});
